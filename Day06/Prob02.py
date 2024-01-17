1a6492bd5110ccfa71755622e9f994b69fb59b76
Kiran <colkiran@gmail.com>
2024-01-17 09:29:23 +0530

    commit

--- Day06/Prob02.py
+++ Day06/Prob02.py
@@ -1,24 +1,33 @@
-
-# l = []
-# for i in range(10):
-#       l.append(input(f"Enter the {i}th string :"))
 #
-# print(l)
-import random
+# # l = []
+# # for i in range(10):
+# #       l.append(input(f"Enter the {i}th string :"))
+# #
+# # print(l)
+# import random
+#
+# l = ['first element', 'second element', 'third element', 'fourth element', 'fifth element', 'sixth element', 'seventh element', 'eight element', 'ninth element', 'tenth element']
+#
+# print(f"l :{l}")
+#
+# l2 = []
+#
+# while len(l2) <= 9:
+#     num = random.randint(0, 9)
+#     if num not in l2:
+#         l2.append(num)
+#
+# FW = open('test.txt', "w")
+# for i in range(len(l2)):
+#     FW.write(l[l2[i]] + "\n")
+#
+# FW.close()
 
-l = ['first element', 'second element', 'third element', 'fourth element', 'fifth element', 'sixth element', 'seventh element', 'eight element', 'ninth element', 'tenth element']
 
-print(f"l :{l}")
+What is likely to be the output after the following statements?
 
-l2 = []
+import time
+y = time.asctime()
+print(y[:3])
 
-while len(l2) <= 9:
-    num = random.randint(0, 9)
-    if num not in l2:
-        l2.append(num)
-
-FW = open('test.txt', "w")
-for i in range(len(l2)):
-    FW.write(l[l2[i]] + "\n")
-
-FW.close()
+'Sat Feb 10 3:40:24 2016'
